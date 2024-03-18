300b25e109af88c40230b410704d11fd32ada111
Yevhen Surfer <23skopets@gmail.com>
2024-03-18 09:34:04 +0100

    minitasks

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+//Task 1: Name Input Program
+
+using System;
+
+class NameInput
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter your name:");
+        string userName = Console.ReadLine(); // Use a descriptive name
+
+        Console.WriteLine($"Hello, {userName}!");
+    }
+}
